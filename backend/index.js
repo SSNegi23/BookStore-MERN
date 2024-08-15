556a11a28fec8baa31a10aa84cb2e017d1f213b6
Shivang <negi1322003@gmail.com>
2024-08-16 00:04:27 +0530

    Created Route: Delete Book

--- backend/index.js
+++ backend/index.js
@@ -99,6 +99,24 @@ app.put('/books/:id', async (req, res) => {
   }
 })
 
+// Route for Delete a book
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    return res.status(200).send({ message: 'Book deleted successfully' });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+})
+
 mongoose.connect(mongoDBURL).then(() => {
   console.log('App connected to database');
   app.listen(PORT, () => {

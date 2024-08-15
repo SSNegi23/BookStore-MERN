7c0af5ecdff96a350083d450653ff2db6d4ba67d
Shivang <negi1322003@gmail.com>
2024-08-15 20:19:01 +0530

    Added MongoDB & mongoose to Node.js | Create Book Model | Created Route for save a book

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,53 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
+// Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send('Welcome to MERN Stack Tutorial');
 });
+
+// Route for Saving a new Book
+app.post('/books', async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message: 'Send all required fields: title, author, publishYear',
+      });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ message: err.message });
+  }
+});
+
+// Route to Get All Books from Database
+
+mongoose.connect(mongoDBURL).then(() => {
+  console.log('App connected to database');
+  app.listen(PORT, () => {
+    console.log(`App is listening to port: ${PORT}`);
+  });
+}).catch((err) => {
+  console.log(err);
+})

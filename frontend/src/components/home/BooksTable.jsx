6a9754a454e5a011419106bf013f4a10631e104a
Shivang <negi1322003@gmail.com>
2024-08-18 16:46:58 +0530

    Deployed

--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -41,10 +41,10 @@ const BooksTable = ({ books }) => {
                   <BsInfoCircle className="text-2xl text-green-800" />
                 </Link>
                 <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-green-800" />
+                  <AiOutlineEdit className="text-2xl text-yellow-600" />
                 </Link>
                 <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className="text-2xl text-green-800" />
+                  <MdOutlineDelete className="text-2xl text-red-600" />
                 </Link>
               </div>
             </td>
